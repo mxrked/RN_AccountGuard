12c83359412b1185677cc0e7709b562d96ad29b2
Parker <pp101socials@gmail.com>
2025-03-13 17:44:15 -0400

    Added

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,8 +1,56 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  Modal,
+  BackHandler,
+} from "react-native";
+import { useRouter, Link } from "expo-router";
+import { useFonts } from "expo-font";
 
 import Global_Styles from "../../assets/styles/Global/Global_Styles";
 import Login_Screen_Styles from "../../assets/styles/Screens/Login_Screen_Styles";
 
+import SettingsModal from "../../assets/components/Global/SettingsModal";
+import TopButtons from "../../assets/components/Global/TopButtons";
+
 export default function Login() {
-  return <View></View>;
+  const router = useRouter();
+
+  const [settingsOpened, setSettingsOpened] = useState(false);
+  const [customFonts] = useFonts({
+    QuicksandBold: require("../../assets/fonts/quicksand/Quicksand-Bold.ttf"),
+  });
+
+  const exitApp = () => {
+    BackHandler.exitApp();
+  };
+
+  const openSettings = () => {
+    setSettingsOpened(true);
+  };
+  const closeSettings = () => {
+    setSettingsOpened(false);
+  };
+
+  return (
+    <ImageBackground
+      source={require("../../assets/imgs/bgs/light-orange-minimal.webp")}
+      style={[Global_Styles.imageBG]}
+    >
+      <View style={[Login_Screen_Styles.darkenOverlay]}>
+        {/** TOP BUTTONS */}
+        <TopButtons openSettings={openSettings} />
+
+        {/** SETTINGS MODAL */}
+        <SettingsModal
+          settingsOpened={settingsOpened}
+          closeSettings={closeSettings}
+        />
+      </View>
+    </ImageBackground>
+  );
 }
--- assets/styles/Screens/Login_Screen_Styles.js
+++ assets/styles/Screens/Login_Screen_Styles.js
@@ -1,5 +1,10 @@
 import { StyleSheet } from "react-native";
 
-const Login_Screen_Styles = StyleSheet.create({});
+const Login_Screen_Styles = StyleSheet.create({
+  darkenOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
+  },
+});
 
 export default Login_Screen_Styles;
